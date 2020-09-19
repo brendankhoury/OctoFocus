92ec71af262c6b8af0d9a363cd2ee4011ee46031
Nicholas Chite <nickman736@gmail.com>
2020-09-18 20:38:11 -0400

    Add project files w/ check window function

---
+++ DefaultMod/ModMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+// 1. Added the "GooseModdingAPI" project as a reference.
+// 2. Compile this.
+// 3. Create a folder with this DLL in the root, and *no GooseModdingAPI DLL*
+using GooseShared;
+using SamEngine;
+
+namespace DefaultMod
+{
+    public class ModEntryPoint : IMod
+    {
+        string activeWindow;
+
+        // Gets called automatically, passes in a class that contains pointers to
+        // useful functions we need to interface with the goose.
+        void IMod.Init()
+        {
+            // Subscribe to whatever events we want
+            InjectionPoints.PostTickEvent += PostTick;
+        }
+
+        public void PostTick(GooseEntity g)
+        {
+            string checkedWindow = GetActiveWindow();
+            if (checkedWindow == null) return;
+            else if (checkedWindow != activeWindow) {
+                activeWindow = checkedWindow;
+
+            }
+
+            // logic
+           
+            // Do whatever you want here.
+            API.Goose.setCurrentTaskByID(g, "FollowMouseDrifty");
+
+            // If we're running our mod's task
+            if (g.currentTask == API.TaskDatabase.getTaskIndexByID("FollowMouseDrifty"))
+            {
+                // Lock our goose facing one direction for some reason?
+                g.direction = 0;
+            }
+        }
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+        string GetActiveWindow()
+        {
+            const int nChars = 256;
+            IntPtr handle;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                Console.WriteLine(Buff.ToString());
+                return Buff.ToString();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DefaultMod/TaskDemo_FollowLowAccel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using GooseShared;
+using SamEngine;
+
+namespace DefaultMod
+{
+    // This is a "Task" class. It defines an AI state for the goose.
+    // It automatically gets indexed by the mod loader due to being a subclass of GooseTaskInfo.
+
+    // Inside, we define a data structure that persists while the task is running.
+    // We also program its 'tick' function, which gets called every frame.
+
+    // 1. Subclass "GooseTaskInfo"
+    class CheckWindowTask : GooseTaskInfo
+    {
+        
+
+        // 2. Construct this task.
+        public CheckWindowTask()
+        {
+            // Should this Task be picked at random by the goose?
+            canBePickedRandomly = false;
+
+            // Describes the task, for readability by other interfaces.
+            shortName = "Check Window task";
+            description = "This is a demo task that checks what application is open.";
+
+            // The key used to access this task in the GooseTaskDatabase:
+            // "Task.GetTaskByID" in the API takes this as an argument.
+            taskID = "CheckWindowTask";
+            // Hot tip: can be nice to set this from a public constant string. Easier access by other parts of your mod.
+        }
+
+
+        // 3. Define the task's 'data' / state.
+        // This defines the 'state' of the Task.
+        public class CheckWindowTaskData : GooseTaskData
+        {
+            public float timeStarted;
+            public string activeWindow;
+        }
+
+        // 4. Override "GetNewTaskData"
+        // Create a 'blank state' on the given goose. Called just before the Task begins running.
+        public override GooseTaskData GetNewTaskData(GooseEntity goose)
+        {
+            CheckWindowTaskData taskData = new CheckWindowTaskData();
+            taskData.timeStarted = Time.time;
+            return taskData;
+        }
+
+        // 4. Override "RunTask"
+        // Run a frame of this Task on the given goose.
+        public override void RunTask(GooseEntity goose)
+        {
+            // This function is only called when we're the currently running task.
+            // The goose's taskData will be of this task's type.
+            CheckWindowTaskData data = (CheckWindowTaskData)goose.currentTaskData;
+
+            goose.currentTaskData.activeWindow = GetActiveWindow();
+        }
+    }
+}
